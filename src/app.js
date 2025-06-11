d8764f6128504d6f2625c3c8e20e89b38f024546
KaitoKidd86 <liritokun1@gmail.com>
2025-06-11 22:08:43 +0000

    mission complete

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+ function generarCartaAleatoria() {
+  const palos = ['diamond', 'heart', 'spade', 'club'];
+  const simbolos = ['♦','♥','♠','♣'];
+  const valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const indicePalo = Math.floor(Math.random() * 4);
+  const indiceValor = Math.floor(Math.random() * 13);
+
+  const palo = palos[indicePalo];
+  const simbolo = simbolos[indicePalo];
+  const valor = valores[indiceValor];
+
+  const cartaDiv = document.getElementById("carta");
+  cartaDiv.className = `card ${palo}`;
+  cartaDiv.innerHTML = `${valor}${simbolo}`;
+ }
+
+ generarCartaAleatoria();
 };
